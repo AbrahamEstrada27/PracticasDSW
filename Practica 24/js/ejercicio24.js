70e00bc3c92bc27ee5028f159d8c431b0cfbcc09
AbrahamEstrada27 <mario.aeg.021@gmail.com>
2023-10-03 18:02:44 -0700

    Primer ejercicio en JAVASCRIPT

---
+++ Practica 24/js/ejercicio24.js
@@ -0,0 +1,30 @@
+// var numero1 = 2;
+// var numero2 = 3;
+// var resultado = numero1 + numero2;
+// alert(resultado);
+
+// var mensaje = "Bienvenidos";
+// var texto1 = "Texto con dobles comillas "
+// var texto2 = "Texto con comillas simples";
+// alert(" "+mensaje+"\n "+texto1+"\n "+texto2);
+
+// var clienteRegistrado = false;
+// var ivaIncluido = true;
+// alert(clienteRegistrado);
+
+// var array = ['tijuana', 'rosarito', 'ensenada', 'mexicali'];
+// alert(array[3]);
+
+var numero1 = prompt("Ingrese un numero:");
+var numero2 = prompt("Ingrese un numero:");
+
+var numero1a = parseInt(numero1);
+var numero2a = parseInt(numero2);
+
+var suma = numero1a + numero2a;
+var resta = numero1a - numero2a;
+var mult = numero1a * numero2a;
+var div = numero1a / numero2a;
+
+alert("Operaciones basicas\n\nSuma: "+suma+"\nResta: "+resta+"\nMultiplicacion: "+mult+"\nDivision: "+div);
+
