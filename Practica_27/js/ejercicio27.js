87b886793f8e7186eb3806e05c5af4140f2b049d
AbrahamEstrada27 <mario.aeg.021@gmail.com>
2023-10-09 19:26:03 -0700

    Ejecicio de JS donde se usa un if mediante un diagrama de flujo

---
+++ Practica_27/js/ejercicio27.js
@@ -0,0 +1,10 @@
+var num1 = 5
+var num2 = 8
+
+if(num1<num2){
+    console.log(`El ${num1} no es mayor que el ${num2}`);
+}else if(num2<= 0){
+    console.log(`El ${num2} es positivo`);
+}else if(++num1<num2){
+    console.log(`Incrementar uno al num1, no lo hace mayor que ${num2}`);
+}
